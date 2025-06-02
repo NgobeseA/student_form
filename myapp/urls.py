2b6ec6f49612be014b8087b8b16f1a4aa2c49a6e
ngobesea <andilekngobese@gmail.com>
2025-06-02 16:07:45 +0200

    update on sign up

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, about, student_registration, student_list
+from .views import home, about, student_registration, student_list, register_view
 
 urlpatterns = [
     path('', home, name='home'),
     path('about/', about, name='about'),
     path('registration/', student_registration, name="student_registration"),
     path('student_list/', student_list, name="student_list"),
+    path('signup/',register_view, name="signup")
 ]
